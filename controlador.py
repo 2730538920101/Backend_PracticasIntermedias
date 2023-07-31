fa3681201e841556c9bc864ffea648cd54e25df2
carlosmz87 <carlosmartz1995@gmail.com>
2023-07-31 00:43:15 -0600

    [ADD] CRUD completo para la lista de contactos

--- controlador.py
+++ controlador.py
@@ -1,1 +1,84 @@
 from conexion import obtener_conexion
+
+#Controlador para registrar contactos en la base de datos
+def RegistrarContacto(nombre, telefono):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        try:
+            cursor.execute("INSERT INTO CONTACTOS(NOMBRE, TELEFONO) VALUES(%s, %s);",(nombre,telefono))
+            conexion.commit()
+            # Validar la inserción
+            if cursor.rowcount > 0:
+                # Inserción exitosa
+                return True
+            else:
+                # Error en la inserción
+                return False
+        except:
+            # Manejo de excepciones en caso de error
+            return False
+        finally:
+            conexion.close()
+
+#Controlador para modificar la informacion del contacto
+def ModificarTelefono(telefono, id):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            query = "UPDATE CONTACTOS SET TELEFONO = %s WHERE ID = %s"
+            cursor.execute(query,(telefono, id))
+            if cursor.rowcount > 0:
+                # La actualización se realizó correctamente
+                conexion.commit()
+                return "SE HA MODIFICADO EXITOSAMENTE EL NUMERO DE TELEFONO DEL CONTACTO SELECCIONADO"
+            else:
+                # No se realizó la actualización
+                return None
+    except:
+        return None
+    finally:
+        conexion.close()
+
+#Controlador para eliminar un contacto
+def EliminarContacto(id):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            query = "DELETE FROM CONTACTOS WHERE ID = %s"
+            cursor.execute(query, (id,))
+            if cursor.rowcount > 0:
+                # La actualización se realizó correctamente
+                conexion.commit()
+                return "SE HA ELIMINADO EXITOSAMENTE EL CONTACTO"
+            else:
+                # No se realizó la actualización
+                return None
+    except:
+        return None
+    finally:
+        conexion.close()
+
+#Controlador para mostrar todos los contactos registrados
+def MostrarContactos():
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            query = "SELECT ID, NOMBRE, TELEFONO FROM CONTACTOS"
+            cursor.execute(query)
+            contactos = cursor.fetchall()
+            if len(contactos) == 0:
+                return None
+            else:
+                contactos_obj = []
+                for contacto in contactos:
+                    contacto_obj = {
+                        "id":contacto[0],
+                        "nombre":contacto[1],
+                        "telefono":contacto[2]
+                    }
+                    contactos_obj.append(contacto_obj)
+                return contactos_obj
+    except:
+        return None
+    finally:
+        conexion.close()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import controlador
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
-
+from conexion import obtener_conexion
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": ["*"]}})
@@ -22,8 +22,83 @@ def index():
         response = make_response(jsonify({'status':'error','PracticasI': "NO SE PUEDE ESTABLECER LA CONEXION A LA BASE DE DATOS"}))
         response.status_code = 500
         return response
-    
 
+#Enpoint para insertar un contacto en la base de datos
+@app.route('/RegistrarContacto', methods=['POST'])
+def RegistrarContacto():
+    try:
+        data = request.get_json()
+        nombre = data['nombre']
+        telefono = data['telefono']
+        flag = controlador.RegistrarContacto(nombre, telefono)
+        if flag:
+            response = make_response(jsonify({'status':'success','PracticasI': "SE HA INSERTADO EL CONTACTO EXITOSMENTE"}))
+            response.status_code = 200
+            return response
+        else:
+            response = make_response(jsonify({'status':'error','PracticasI': "NO SE HA PODIDO INSERTAR EL NUEVO CONTACTO"}))
+            response.status_code = 400
+            return response
+    except:
+        response = make_response(jsonify({'status':'error','PracticasI': "ERROR DE CONEXION CON EL SERVIDOR"}))
+        response.status_code = 500
+        return response
+
+#Endpoint para modificar el numero de telefono de un contacto
+@app.route('/ModificarTelefono/<id>', methods=['PUT'])
+def ModificarTelefono(id):
+    try:
+        data = request.get_json()
+        telefono = data['telefono']
+        flag = controlador.ModificarTelefono(telefono, id)
+        if flag is not None:
+            response = make_response(jsonify({'status':'success','PracticasI': flag}))
+            response.status_code = 200
+            return response
+        else:
+            response = make_response(jsonify({'status':'error','PracticasI': "NO SE HA PODIDO ACTUALIZAR EL NUMERO DE TELEFONO DEL CONTACTO"}))
+            response.status_code = 400
+            return response
+    except:
+        response = make_response(jsonify({'status':'error','PracticasI': "ERROR DE CONEXION CON EL SERVIDOR"}))
+        response.status_code = 500
+        return response
+
+#Endpoint para Eliminar un contacto de la base de datos
+@app.route('/EliminarContacto/<id>', methods=['DELETE'])
+def EliminarContacto(id):
+    try:
+        flag = controlador.EliminarContacto(id)
+        if flag is not None:
+            response = make_response(jsonify({'status':'success','PracticasI': flag}))
+            response.status_code = 200
+            return response
+        else:
+            response = make_response(jsonify({'status':'error','PracticasI': "NO SE HA PODIDO ELIMINAR EL CONTACTO"}))
+            response.status_code = 400
+            return response
+    except:
+        response = make_response(jsonify({'status':'error','PracticasI': "ERROR DE CONEXION CON EL SERVIDOR"}))
+        response.status_code = 500
+        return response
+    
+#Endpoint para mostrar todos los contactos
+@app.route('/MostrarContactos', methods=['GET'])
+def MostrarContactos():
+    try:
+        contactos = controlador.MostrarContactos()
+        if contactos is not None:
+            response = make_response(jsonify({'status':'success', 'PracticasI':contactos}))
+            response.status_code = 200
+            return response
+        else:
+            response = make_response(jsonify({'status':'error', 'PracticasI':'ERROR AL OBTENER LOS CONTACTOS'}))
+            response.status_code = 400
+            return response  
+    except:
+        response = make_response(jsonify({'status':'error','PracticasI': 'ERROR DE COMUNICACION'}))
+        response.status_code = 500
+        return response
     
 if __name__ == '__main__':
     print("SERVIDOR INICIADO EN EL PUERTO: 5000")
